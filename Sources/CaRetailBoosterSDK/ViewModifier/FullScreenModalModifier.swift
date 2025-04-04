b8fb99e3e150e43d3cca6a05e4d81a0c85c1cdfe
Masaaki Yoda <01ewell0@gmail.com>
2025-04-04 15:51:56 +0900

    fix modal safe area display

--- Sources/CaRetailBoosterSDK/ViewModifier/FullScreenModalModifier.swift
+++ Sources/CaRetailBoosterSDK/ViewModifier/FullScreenModalModifier.swift
@@ -36,10 +36,14 @@ struct FullScreenModal<Content: View>: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         if isPresented {
             if context.coordinator.modalViewController == nil {
-                let hostingController = UIHostingController(rootView: content())
+                let modalContent = content()
+                    .edgesIgnoringSafeArea(.all)
+                
+                let hostingController = UIHostingController(rootView: modalContent)
                 hostingController.modalPresentationStyle = .overFullScreen
+                hostingController.disableSafeArea()
                 hostingController.presentationController?.delegate = context.coordinator
-                uiViewController.present(hostingController, animated: false) {
+                uiViewController.present(hostingController, animated: true) {
                     context.coordinator.modalViewController = hostingController
                 }
             }
@@ -68,6 +72,30 @@ struct FullScreenModal<Content: View>: UIViewControllerRepresentable {
         func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
             parent.isPresented = false
             modalViewController = nil
+        }
+    }
+}
+
+extension UIHostingController {
+    func disableSafeArea() {
+        guard let viewClass = object_getClass(view) else { return }
+        
+        let viewSubclassName = String(cString: class_getName(viewClass)).appending("_IgnoreSafeArea")
+        if let viewSubclass = NSClassFromString(viewSubclassName) {
+            object_setClass(view, viewSubclass)
+        } else {
+            guard let viewClassNameUtf8 = (viewSubclassName as NSString).utf8String else { return }
+            guard let viewSubclass = objc_allocateClassPair(viewClass, viewClassNameUtf8, 0) else { return }
+            
+            if let method = class_getInstanceMethod(UIView.self, #selector(getter: UIView.safeAreaInsets)) {
+                let safeAreaInsets: @convention(block) (AnyObject) -> UIEdgeInsets = { _ in
+                    return .zero
+                }
+                class_addMethod(viewSubclass, #selector(getter: UIView.safeAreaInsets), imp_implementationWithBlock(safeAreaInsets), method_getTypeEncoding(method))
+            }
+            
+            objc_registerClassPair(viewSubclass)
+            object_setClass(view, viewSubclass)
         }
     }
 }
